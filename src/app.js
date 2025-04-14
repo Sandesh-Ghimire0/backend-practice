cca46e754827bdd0caf8b7e96e78e5ff3fe5ca3f
Sandesh-Ghimire0 <sandeshghimire1000@gmail.com>
2025-04-14 10:46:58 +0545

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,3 +14,11 @@ app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true, limit:'16kb'}))
 app.use(express.static('public'))
 app.use(cookieParser()) // it is used to read and write the cookies from the user browser
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+import userRouter from './routes/user.route.js'
+
+app.use('/api/v1/users',userRouter)
+// http://localhost:8000/api/v1/users/<user.route - methods>register
